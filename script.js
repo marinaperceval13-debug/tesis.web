48096c29eb36b6f4c70caefef2c59c542a3f621d
marinaperceval13-debug <marinaperceval13@gmail.com>
2026-01-14 04:33:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const PASSWORD_HASH = "abb634e86d43122098486bcea800a3da"; // Hash de 'bat2026'
+
+  let pass = prompt("Introduce thepassword to access:");
+
+  async function md5(message) {
+    const msgUint8 = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest("MD5", msgUint8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
+  }
+
+  md5(pass).then(hash => {
+    if (hash !== PASSWORD_HASH) {
+      alert("Contraseña incorrecta. Acceso denegado.");
+      document.body.innerHTML = "<h1 style='text-align:center;margin-top:20%;color:#facc15;'>Acceso restringido 🔒</h1>";
+    }
+  });
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   const links = document.querySelectorAll("nav a");
   const contenido = document.getElementById("contenido");
